9506ec483506e39cdbac6adc24136a6df9900d2a
Luca Stefani <luca020400@lineageos.org>
2017-08-17 21:21:23 +0200

    Add ELYTRA build support.
    Squashed also with the following changes:
    
      Author: Simon Shields <simon@lineageos.org>
      Date:   Fri Mar 2 12:55:47 2018 +1100
    
        build: Unconditionally use ELYTRA pathmap.
    
        even if we're not building a lineage target, we need to
        use the lineage pathmap to make things like recovery and ril
        happy
    
      Change-Id: I974c30ad10d4ff5b7805c3df9c22010f1e002bf1
    
      Author: Luca Stefani <luca.stefani.ge1@gmail.com>
      Date:   Tue Jan 22 15:50:52 2019 +0100
    
        Don't export BUILD_NUMBER in envsetup.
    
        * The value is meant to be set only before running an /official/ build
        * This allows us to execute consecutive make commands without
          running kati for every invocation
    
      Change-Id: Ifa94b839f30c7260009c8a3c91c202b50e28022e
    
    Change-Id: I6ca3c6a68aa5ea78649b96ac86fb65ff7f489736

--- envsetup.sh
+++ envsetup.sh
@@ -147,6 +147,13 @@ function check_product()
         echo "Couldn't locate the top of the tree.  Try setting TOP." >&2
         return
     fi
+    if (echo -n $1 | grep -q -e "^elytra_") ; then
+        ELYTRA_BUILD=$(echo -n $1 | sed -e 's/^elytra_//g')
+    else
+        ELYTRA_BUILD=
+    fi
+    export ELYTRA_BUILD
+
         TARGET_PRODUCT=$1 \
         TARGET_BUILD_VARIANT= \
         TARGET_BUILD_TYPE= \
@@ -754,6 +761,8 @@ function lunch()
         echo "Invalid lunch combo: $selection"
         return 1
     fi
+
+    check_product $product
 
     TARGET_PRODUCT=$product \
     TARGET_BUILD_VARIANT=$variant \
